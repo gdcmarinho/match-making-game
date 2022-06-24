ca6f37a6ca5e52f4cfeab7896246f147b95f5387
Gabriel Marinho <gdcmarinho@gmail.com>
2022-06-24 09:40:28 -0300

    refactor: break routine in functions

--- src/game-client/repository/rabbitmq.go
+++ src/game-client/repository/rabbitmq.go
@@ -6,26 +6,13 @@ import (
 	"github.com/streadway/amqp"
 )
 
-func Publish() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+func init() {
+	startConnection()
+	declareQueue()
+	openChannel()
+}
 
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-
-	failOnError(err, "Failed to declare a queue")
-
+func Publish() {	
 	body := "Someone is trying to play a match"
 	err = ch.Publish(
 		"",     // exchange
@@ -38,6 +25,30 @@ func Publish() {
 		})
 	failOnError(err, "Failed to publish a requisition")
 	log.Printf(" [x] Sent %s\n", body)
+}
+
+func startConnection() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+}
+
+func openChannel() {
+	ch, err = conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+}
+
+func declareQueue() {
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
 }
 
 func failOnError(err error, msg string) {
