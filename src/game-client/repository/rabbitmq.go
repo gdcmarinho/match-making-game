2a8111524417de80515c027639a3361ec04568bf
Gabriel Marinho <gdcmarinho@gmail.com>
2022-07-01 01:27:58 +0000

    fix(#1): fix broke connection and publishing routine

--- src/game-client/repository/rabbitmq.go
+++ src/game-client/repository/rabbitmq.go
@@ -6,15 +6,19 @@ import (
 	"github.com/streadway/amqp"
 )
 
+var conn *amqp.Connection
+var ch *amqp.Channel
+var q amqp.Queue
+
 func init() {
-	startConnection()
-	declareQueue()
-	openChannel()
+	conn = startConnection()
+	ch = openChannel(conn)
+	q = declareQueue(ch)
 }
 
 func Publish() {	
 	body := "Someone is trying to play a match"
-	err = ch.Publish(
+	err := ch.Publish(
 		"",     // exchange
 		q.Name, // routing key
 		false,  // mandatory
@@ -23,36 +27,46 @@ func Publish() {
 			ContentType: "text/plain",
 			Body:        []byte(body),
 		})
-	failOnError(err, "Failed to publish a requisition")
+
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to publish a requisition", err)
+	}
+
 	log.Printf(" [x] Sent %s\n", body)
 }
 
-func startConnection() {
+func startConnection() *amqp.Connection {
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to connect to RabbitMQ", err)
+	}
+
 	defer conn.Close()
+	return conn
 }
 
-func openChannel() {
-	ch, err = conn.Channel()
-	failOnError(err, "Failed to open a channel")
+func openChannel(conn *amqp.Connection) *amqp.Channel {
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to open a channel", err)
+	}
+
 	defer ch.Close()
+	return ch
 }
 
-func declareQueue() {
+func declareQueue(ch *amqp.Channel) amqp.Queue {
 	q, err := ch.QueueDeclare(
 		"hello", // name
-		false,   // durable
+		true,   // durable
 		false,   // delete when unused
 		false,   // exclusive
 		false,   // no-wait
 		nil,     // arguments
 	)
-	failOnError(err, "Failed to declare a queue")
-}
-
-func failOnError(err error, msg string) {
 	if err != nil {
-	  log.Fatalf("%s: %s", msg, err)
+		log.Fatalf("%s: %s", "Failed to declare a queue", err)
 	}
+
+	return q
 }
